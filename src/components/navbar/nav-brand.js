bee5e95ff9ff4bda17c11ea3e83bb86d51017cce
Ievgen Sein <xdiego.pwu@gmail.com>
2018-12-01 15:41:24 +0200

    Replaced nav brand elements with an image

--- src/components/navbar/nav-brand.js
+++ src/components/navbar/nav-brand.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import fp from '../../images/fp.png';
+// import fp from '../../images/fp.png';
+import fp from '../../images/fp-logo-2x.png';
 
-const FP = styled.img`
-	width: 65px;
-	height: auto;
-	margin-top: 10px;
-`;
-
-const BrandName = styled.p`
-	color: #fff;
-	font-size: 35px;
-	font-family: Amiri;
-	font-style: italic;
-	font-weight: bold;
-	letter-spacing: 1.2px;
-	margin-bottom: 0;
-	line-height: 1;
-	margin-top: 14px;
-`;
-
-const BrandPhrase = styled.p`
-	color: #fff;
-	font-size: 14px;
-	font-family: DejaVu Math TeX Gyre;
-	margin-bottom: 0;
+const Fp = styled.img`
+	max-width: 100%;
 `;
 
 const NavBrand = () => (
 	<div className="row">
-		<div className="col-2 offset-2">
-			<FP src={fp} />
-		</div>
-		<div className="col-8">
-			<BrandName> FARMA PALATIA </BrandName>
-			<BrandPhrase> Εργαστήριο αποξηραμένων φρούτων </BrandPhrase>
+		<div className="col-10 offset-2">
+			<Fp className="image-fluid mt-2" src={fp} alt="fp-logo-brand" />
 		</div>
 	</div>
 );
