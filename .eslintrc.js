06e026874c59683fb5902c818046ccf44730e74c
Ievgen Sein <xdiego.pwu@gmail.com>
2018-12-06 20:41:50 +0200

    turned no-dublicates into a warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
 		'import/no-unresolved': 1,
 		'import/no-webpack-loader-syntax': 0,
 		'import/prefer-default-export': 0,
+		'import/no-duplicates': 0,
 		'jsx-a11y/anchor-is-valid': [
 			'warning',
 			{
