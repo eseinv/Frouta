ff1dc88e61d009cb7d3ac341290043d927297de1
Ievgen Sein <xdiego.pwu@gmail.com>
2018-12-25 14:08:49 +0200

    fixed images paths

--- src/main-product-list.js
+++ src/main-product-list.js
@@ -4,41 +4,41 @@ export const MainProductList = [
 		name: 'Πεστίλη ντομάτας',
 		info:
 			'Η πεστίλη παράγεται σε ένα πολύ μεγάλο φούρνο που καίει πολύ ρεύμα. Αγόρασε την τώρα.',
-		image: './images/ntomata.png',
+		image: '../images/ntomata.png',
 	},
 	{
 		id: '2',
 		name: 'Πεστίλη μήλου',
 		info:
 			'Η πεστίλη παράγεται σε ένα πολύ μεγάλο φούρνο που καίει πολύ ρεύμα. Αγόρασε την τώρα.',
-		image: './images/yabloko.jpg',
+		image: '../images/yabloko.png',
 	},
 	{
 		id: '3',
 		name: 'Σούπα με μανιτάρια',
 		info:
 			'Η πεστίλη παράγεται σε ένα πολύ μεγάλο φούρνο που καίει πολύ ρεύμα. Αγόρασε την τώρα.',
-		image: './images/soup.jpg',
+		image: '../images/soup.jpg',
 	},
 	{
 		id: '1',
 		name: 'Πεστίλη ντομάτας',
 		info:
 			'Η πεστίλη παράγεται σε ένα πολύ μεγάλο φούρνο που καίει πολύ ρεύμα. Αγόρασε την τώρα.',
-		image: './images/ntomata.png',
+		image: '../images/ntomata.png',
 	},
 	{
 		id: '2',
 		name: 'Πεστίλη μήλου',
 		info:
 			'Η πεστίλη παράγεται σε ένα πολύ μεγάλο φούρνο που καίει πολύ ρεύμα. Αγόρασε την τώρα.',
-		image: './images/yabloko.jpg',
+		image: '../images/yabloko.jpg',
 	},
 	{
 		id: '3',
 		name: 'Σούπα με μανιτάρια',
 		info:
 			'Η πεστίλη παράγεται σε ένα πολύ μεγάλο φούρνο που καίει πολύ ρεύμα. Αγόρασε την τώρα.',
-		image: './images/soup.jpg',
+		image: '../images/soup.jpg',
 	},
 ];
