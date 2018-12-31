cda13031d42be4600484532fb935a67bb511483d
Ievgen Sein <xdiego.pwu@gmail.com>
2018-12-31 04:54:13 +0200

    updated logged state name to userLogged

--- src/index.js
+++ src/index.js
@@ -6,25 +6,22 @@ import { Contact } from './components/contact';
 import { NavBar } from './components/navbar/nav-bar';
 import { ProductList } from './components/product-list';
 import { Footer } from './components/footer';
+import { Login } from './components/login';
 
 class App extends React.Component {
-	state = { logState: false };
-	toggleLog = () => {
-		this.setState({ logState: !this.state.logState });
-	};
+	state = { userLogged: false };
+
 	render() {
 		return (
 			<div>
 				<Router>
 					<div>
-						<NavBar
-							toggleLog={this.toggleLog}
-							logState={this.state.logState}
-						/>
+						<NavBar userLogged={this.state.userLogged} />
 
 						<Route exact path="/" component={ProductList} />
 						<Route path="/product/:id" component={Product} />
 						<Route path="/contact" component={Contact} />
+						<Route path="/login" component={Login} />
 					</div>
 				</Router>
 				<Footer />
