35cd56fc55cc4b83b785c598e53497ef1239cce4
pbeocanin <pbeocanin@gmail.com>
2019-01-01 18:47:18 +0100

    Updates

--- src/components/login.js
+++ src/components/login.js
@@ -14,12 +14,15 @@ const Input = styled.input`
 	border-top-width: 0;
 	border-left-width: 0;
 	border-right-width: 0;
-	width: 130px;
-	text-align: center;
-	background: #F6F8F8
-	color: #707070
+	background: #f6f8f8;
+	color: #707070;
 	&:focus {
 		outline: none;
+		box-shadow: none;
+		border-top-color: transparent;
+		border-left-color: transparent;
+		border-right-color: transparent;
+		border-bottom-color: #587c34;
 	}
 	&::selection {
 		background: #587c34;
@@ -36,15 +39,21 @@ const LogError = styled.div`
 
 const LogButton = styled.button`
 	border: 1px solid #587c34;
-	width: 130px;
-	background: #fff;
+	background-image: linear-gradient(
+		rgba(255, 255, 255, 1),
+		rgba(255, 255, 255, 1)
+	);
 	color: #587c34;
-	padding: 3px;
+	cursor: pointer;
 	&:focus {
 		outline: none;
 	}
 	&:hover {
-		cursor: pointer;
+		background-image: linear-gradient(
+				rgba(0, 0, 0, 0.05),
+				rgba(0, 0, 0, 0.05)
+			),
+			linear-gradient(rgba(255, 255, 255, 1), rgba(255, 255, 255, 1));
 	}
 `;
 
@@ -52,11 +61,10 @@ class Login extends React.Component {
 	state = { usernameField: '', passwordField: '', logError: false };
 
 	handleInputChange = (value, fieldType) => {
-		if (fieldType === 'username') {
-			this.setState({ usernameField: value });
-		} else if (fieldType === 'password') {
-			this.setState({ passwordField: value });
-		}
+		const target = `${fieldType}Field`;
+		this.setState({
+			[target]: value,
+		});
 	};
 
 	handleSubmit = (event, userEntered, passEntered) => {
@@ -69,8 +77,9 @@ class Login extends React.Component {
 		const [userFound] = checkUser;
 
 		if (userFound) {
-			this.setState({ logError: false });
-		} else this.setState({ logError: true });
+			return this.setState({ logError: false });
+		}
+		return this.setState({ logError: true });
 	};
 
 	render() {
@@ -81,58 +90,65 @@ class Login extends React.Component {
 						<H3>Login</H3>
 					</div>
 				</div>
-				<div className="row">
-					<form
-						className="col-12"
-						onSubmit={e =>
-							this.handleSubmit(
-								e,
-								this.state.usernameField,
-								this.state.passwordField,
-							)
-						}
-					>
-						<div className="text-center">
-							<label className="d-block" htmlFor="username" />
-							<Input
-								onChange={event =>
-									this.handleInputChange(
-										event.target.value,
-										'username',
-									)
-								}
-								value={this.state.usernameField}
-								type="text"
-								placeholder="Username"
-							/>
-						</div>
-						<div className="text-center mt-3">
-							<label className="d-block" htmlFor="password" />
-							<Input
-								onChange={event =>
-									this.handleInputChange(
-										event.target.value,
-										'password',
-									)
-								}
-								value={this.state.passwordField}
-								type="password"
-								placeholder="Password"
-							/>
-						</div>
-						{this.state.logError === true && (
-							<LogError className="text-center">
-								Incorrect credentials
-							</LogError>
-						)}
-						<div className="col-12 text-center mt-4">
-							<LogButton>Login</LogButton>
-						</div>
-					</form>
+				<div className="row justify-content-around">
+					<div className="col-4">
+						<form
+							onSubmit={e =>
+								this.handleSubmit(
+									e,
+									this.state.usernameField,
+									this.state.passwordField,
+								)
+							}
+						>
+							<div className="form-group">
+								<label className="d-block" htmlFor="username">
+									User
+								</label>
+								<Input
+									onChange={event =>
+										this.handleInputChange(
+											event.target.value,
+											'username',
+										)
+									}
+									value={this.state.usernameField}
+									type="text"
+									placeholder="Username"
+									className="form-control"
+								/>
+							</div>
+							<div className="form-group">
+								<label className="d-block" htmlFor="password">
+									Pass
+								</label>
+								<Input
+									onChange={event =>
+										this.handleInputChange(
+											event.target.value,
+											'password',
+										)
+									}
+									value={this.state.passwordField}
+									type="password"
+									placeholder="Password"
+									className="form-control"
+								/>
+							</div>
+							{this.state.logError === true && (
+								<LogError className="text-center">
+									Incorrect credentials
+								</LogError>
+							)}
+							<LogButton className="btn-block mt-4 py-2">
+								Log in
+							</LogButton>
+						</form>
+					</div>
 				</div>
 			</div>
 		);
 	}
 }
 
-export { Login };
+export default Login;
