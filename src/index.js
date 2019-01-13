c8a616fb4da510e4ba96a6cf843ad39d810226a3
Ievgen Sein <xdiego.pwu@gmail.com>
2019-01-13 03:55:48 +0200

    log in/out funcionality and recovery of logged state on revisit

--- src/index.js
+++ src/index.js
@@ -10,18 +10,46 @@ import Login from './components/login';
 
 class App extends React.Component {
 	state = { userLogged: false };
+	logUserIn = () => {
+		this.setState({ userLogged: true }, () =>
+			localStorage.setItem('userLogged', 'true'),
+		);
+	};
+	logUserOut = () => {
+		this.setState({ userLogged: false }, () =>
+			localStorage.removeItem('userLogged'),
+		);
+	};
+	componentWillMount() {
+		const savedLogState = localStorage.getItem('userLogged');
+		if (savedLogState === 'true') {
+			this.setState({ userLogged: true });
+		}
+	}
 
 	render() {
 		return (
 			<div>
 				<Router>
 					<div>
-						<NavBar userLogged={this.state.userLogged} />
+						<NavBar
+							userLogged={this.state.userLogged}
+							logUserOut={this.logUserOut}
+						/>
 
 						<Route exact path="/" component={ProductList} />
 						<Route path="/product/:id" component={Product} />
 						<Route path="/contact" component={Contact} />
-						<Route path="/login" component={Login} />
+						<Route
+							path="/login"
+							render={props => (
+								<Login
+									{...props}
+									userLogged={this.state.userLogged}
+									logUserIn={this.logUserIn}
+								/>
+							)}
+						/>
 					</div>
 				</Router>
 				<Footer />
