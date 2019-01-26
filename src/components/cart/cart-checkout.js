855856e9c8ac5f76dc3eee798b2a64c9c38f92dc
Ievgen Sein <xdiego.pwu@gmail.com>
2019-01-26 21:40:14 +0200

    showing total amount to pay in checkout

--- src/components/cart/cart-checkout.js
+++ src/components/cart/cart-checkout.js
@@ -1,3 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const CartCheckout = () => <div>CartCheckout component</div>;
+class CartCheckout extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { finalCartPrice: 0 };
+	}
+
+	updatePrices = prices => {
+		this.setState({ finalCartPrice: prices });
+	};
+
+	render() {
+		const priceArray = [];
+		this.props.cart.map((cartItem, index) =>
+			priceArray.push(this.props.cart[index].totalPrice),
+		);
+		const prices = priceArray.reduce((sum, price) => sum + price, 0);
+		if (prices !== this.state.finalCartPrice) {
+			this.setState({ finalCartPrice: prices });
+		}
+		return (
+			<div className="sticky-top text-center h3 p-3 mt-2 card">
+				Σύνολο προς πληρωμή:{' '}
+				<strong className="mt-2">
+					{this.state.finalCartPrice} &euro;
+				</strong>
+			</div>
+		);
+	}
+}
+
+CartCheckout.propTypes = {
+	cart: PropTypes.array,
+};
+export default CartCheckout;
