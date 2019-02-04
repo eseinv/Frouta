1f72d0737710eeb91d6fa42d75acbb65d5235082
Ievgen Sein <xdiego.pwu@gmail.com>
2019-02-05 00:15:47 +0200

    small css fix

--- src/components/cart/cart-checkout.js
+++ src/components/cart/cart-checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormButton } from '../form-button';
 
 class CartCheckout extends React.Component {
 	constructor(props) {
@@ -22,10 +23,25 @@ class CartCheckout extends React.Component {
 		}
 		return (
 			<div className="sticky-top text-center h3 p-3 mt-2 card">
-				Σύνολο προς πληρωμή:{' '}
-				<strong className="mt-2">
-					{this.state.finalCartPrice} &euro;
-				</strong>
+				<div className="row">
+					<div className="col-12 font-weight-light">
+						Σύνολο προς πληρωμή:
+					</div>
+				</div>
+				<div className="row mt-2">
+					<div className="col-12">
+						<span className="mt-2 h1 font-weight-bold">
+							{this.state.finalCartPrice} &euro;
+						</span>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12">
+						<FormButton width={200} className="text-center h4">
+							Επιβεβαίωση
+						</FormButton>
+					</div>
+				</div>
 			</div>
 		);
 	}
