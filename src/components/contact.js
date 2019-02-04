8e12ed40e6e766117bad1cb1615349fd59ed8242
Ievgen Sein <xdiego.pwu@gmail.com>
2019-02-05 00:16:22 +0200

    remade and translated

--- src/components/contact.js
+++ src/components/contact.js
@@ -28,60 +28,26 @@ const Contact = () => (
 		<div className="container">
 			<div className="row">
 				<div className="col-12 mt-5 mb-3 text-center">
-					<H3>Contact Information</H3>
+					<H3>Στοιχεία Επικοινωνίας</H3>
 				</div>
 			</div>
 			<div className="row">
-				<div className="col-6 text-right px-4">
-					<H5>Company HQ</H5>
-					<P>Black Action Street 4, NY</P>
-				</div>
-				<div className="col-6 text-left px-4">
-					<H5>Important Person</H5>
-					<P>youwantit@contact.hq</P>
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-6 text-right px-4">
-					<H5>Landline</H5>
-					<P>555-SALES-PPL</P>
-				</div>
-				<div className="col-6 text-left px-4">
-					<H5>Sales represantative</H5>
-					<P>jfletcher@contact.hq</P>
+				<div className="col-12 text-center border mb-4">
+					<P>Μηλιές Πηλίου, Νομός Θεσσαλίας</P>
+					<P>Τηλέφωνο: 6945296507</P>
 				</div>
 			</div>
 
-			<div className="row">
-				<div className="col-6 text-right px-4">
-					<H5>Disputes</H5>
-					<P>sumtinwong@contact.hq</P>
-				</div>
-				<div className="col-6 text-left px-4">
-					<H5>Technical Issues</H5>
-					<P>bolimekura@contact.hq</P>
-				</div>
-			</div>
-
-			<div className="row">
-				<div className="col-12">
-					<P className="mt-4 text-center">
-						You can visit us during regular working hours. Our
-						friendly customer support is always happy to help!
-					</P>
-				</div>
-			</div>
-		</div>
-
-		<div className="container-fluid">
-			<div className="row">
-				<div className="col-12">
-					<H5 className="text-center">Find us here</H5>
-					<img
-						className="img-fluid mx-auto d-block"
-						src={map}
-						alt="map"
-					/>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-12">
+						<H5 className="text-center">Βρείτε μας εδώ</H5>
+						<img
+							className="img-fluid mx-auto d-block"
+							src={map}
+							alt="map"
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
