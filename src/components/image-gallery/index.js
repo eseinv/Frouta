7b26d2ea38cd09bd52aabbde8c3283098ed796f4
Ievgen Sein <xdiego.pwu@gmail.com>
2019-03-03 12:25:47 +0200

    Checking if images have been loaded before showing them

--- src/components/image-gallery/index.js
+++ src/components/image-gallery/index.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
+import Loader from 'react-loader-spinner';
+import ImagesLoaded from 'react-images-loaded';
 import PropTypes from 'prop-types';
 
-const Gallery = props => (
-	<div className="container mt-2 mb-4">
-		<ImageGallery
-			items={props.images}
-			infinite
-			showThumbnails={false}
-			showPlayButton={false}
-			showFullscreenButton={false}
-			showBullets
-			autoPlay
-			showNav={false}
-			slideDuration={2700}
-		/>
-	</div>
-);
+class Gallery extends React.Component {
+	state = { loading: true };
+	isLoaded = () => {
+		this.setState({ loading: false });
+	};
+	render() {
+		return (
+			<div className="container mt-2 mb-4">
+				{this.state.loading ? (
+					<div>
+						<div className="d-flex justify-content-center">
+							<Loader
+								type="Oval"
+								color="#27ae60"
+								height={80}
+								width={80}
+							/>
+						</div>
+						<p className="text-center">Loading gallery</p>
+					</div>
+				) : null}
+				<ImagesLoaded done={this.isLoaded}>
+					<ImageGallery
+						items={this.props.images}
+						infinite
+						showThumbnails={false}
+						showPlayButton={false}
+						showFullscreenButton={false}
+						showBullets={!this.state.loading}
+						autoPlay
+						showNav={false}
+						slideDuration={2700}
+					/>
+				</ImagesLoaded>
+			</div>
+		);
+	}
+}
 
 Gallery.propTypes = {
 	images: PropTypes.array,
