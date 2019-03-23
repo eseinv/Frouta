4571230bb77490efc863434f31ead0475724d02c
Ievgen Sein <xdiego.pwu@gmail.com>
2019-03-23 10:04:22 +0200

    moved all gallery images src to a single file

---
+++ src/data/images.js
@@ -0,0 +1,50 @@
+export const Images = [
+	{
+		original: `http://homestead.test/images/gallery/1.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/2.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/3.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/4.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/5.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/6.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/7.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/8.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/9.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/10.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/11.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/12.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/13.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/14.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/15.jpg`,
+	},
+	{
+		original: `http://homestead.test/images/gallery/16.jpg`,
+	},
+];
