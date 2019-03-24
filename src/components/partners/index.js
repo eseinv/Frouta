689e6cd24d37fb2d19d687e6f12f3f59b50a201e
Ievgen Sein <xdiego.pwu@gmail.com>
2019-03-24 11:11:35 +0200

    created partners page

---
+++ src/components/partners/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { H3 } from '../contact/style';
+
+const Partners = () => (
+	<div className="container">
+		<div className="row">
+			<div className="col-12 mt-5 mb-3 text-center">
+				<H3>Οι Συνεργάτες Μας</H3>
+			</div>
+		</div>
+		<div className="row mt-4">
+			<div className="col-12 col-md-4">
+				<img
+					src="http://homestead.test/images/partners/partner.jpg"
+					alt="Product Name"
+					className="img-fluid"
+				/>
+			</div>
+			<div className="col-12 col-md-4">
+				<img
+					src="http://homestead.test/images/partners/partner.jpg"
+					alt="Product Name"
+					className="img-fluid"
+				/>
+			</div>
+			<div className="col-12 col-md-4">
+				<img
+					src="http://homestead.test/images/partners/partner.jpg"
+					alt="Product Name"
+					className="img-fluid"
+				/>
+			</div>
+		</div>
+		<hr />
+
+		<div className="row mt-3">
+			<div className="col-12 col-md-4">
+				<img
+					src="http://homestead.test/images/partners/partner.jpg"
+					alt="Product Name"
+					className="img-fluid"
+				/>
+			</div>
+			<div className="col-12 col-md-4">
+				<img
+					src="http://homestead.test/images/partners/partner.jpg"
+					alt="Product Name"
+					className="img-fluid"
+				/>
+			</div>
+			<div className="col-12 col-md-4">
+				<img
+					src="http://homestead.test/images/partners/partner.jpg"
+					alt="Product Name"
+					className="img-fluid"
+				/>
+			</div>
+		</div>
+	</div>
+);
+
+export { Partners };
