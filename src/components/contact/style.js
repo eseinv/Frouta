a0a50a54295cb18f950b47e1b474d8fc73c48111
Ievgen Sein <xdiego.pwu@gmail.com>
2019-04-28 18:43:54 +0300

    edited contact styling

--- src/components/contact/style.js
+++ src/components/contact/style.js
@@ -8,14 +8,20 @@ export const H3 = styled.h3`
 `;
 
 export const H5 = styled.h5`
-	color: #707070;
+	padding-top: 10px;
+	color: #587c34;
 	&:hover {
 		cursor: default;
 	}
 `;
 
+export const Subtitle = styled(H5)`
+	padding: 0 0 10px 0;
+`;
+
 export const P = styled.p`
 	color: #373535;
+	font-style: italic;
 	&:hover {
 		cursor: default;
 	}
