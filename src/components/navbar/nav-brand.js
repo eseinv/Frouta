01c1e953d3ede3f3d3018c3d7290674933be535f
Ievgen Sein <eseinv@gmail.com>
2019-06-04 20:53:27 +0300

    removed obsolete logo

--- src/components/navbar/nav-brand.js
+++ src/components/navbar/nav-brand.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { FpBig, FpSmall } from './style';
+import { FpBig } from './style';
 
 const NavBrand = () => (
 	<div className="ml-4 p-2">
 		<FpBig
 			className="img-fluid"
 			src="https://api.farmapalatia.gr/images/fp-logo-2x.png"
-			alt="fp-logo-brand"
-		/>
-		<FpSmall
-			className="img-fluid mt-2"
-			src="https://api.farmapalatia.gr/images/FINAL_fp_logo@2x.png"
 			alt="fp-logo-brand"
 		/>
 	</div>
