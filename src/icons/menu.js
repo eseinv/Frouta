b90e7b196816f91dc514e327c6359d82c76af456
Ievgen Sein <eseinv@gmail.com>
2019-06-04 20:52:23 +0300

    updated burger menu icon

--- src/icons/menu.js
+++ src/icons/menu.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Menu = props => (
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width="15px">
-		<path
-			d="M112 6H12C5.4 6 0 11.4 0 18s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12zM112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12zM112 94H12c-6.6 0-12 5.4-12 12s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z"
-			fill={props.color}
-		/>
+	<svg xmlns="http://www.w3.org/2000/svg" width="22" height="15">
+		<g fill={props.color} stroke="#62534B">
+			<rect width="21" height="2" x=".5" y=".5" rx=".5" />
+			<rect width="21" height="2" x=".5" y="6.5" rx=".5" />
+			<rect width="21" height="2" x=".5" y="12.5" rx=".5" />
+		</g>
 	</svg>
 );
 
